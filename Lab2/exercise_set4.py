2966d999fc4f1c011eae83502e377d174c7e2840
Klejd1 <s335125@studenti.polito.it>
2024-10-18 22:55:10 +0200

    ./

--- Lab2/exercise_set4.py
+++ Lab2/exercise_set4.py
@@ -1,1 +1,32 @@
+import pandas as pd
+import matplotlib as plt
 
+#1. Read Total profit of all months and show it using a line plot
+def exercise1():
+    sales = pd.read_csv('sales_data.csv')
+    print("Yes")
+
+
+#2. Get Total profit of all months and show line plot with the following Style properties:
+#label = ’Profit data of last year’; color=’r’; marker=’o’;
+#markerfacecolor=’k’; linestyle=’–’; linewidth=3.
+
+def exercise2():
+    pass
+#3. Read all product sales data and show it using a multiline plot
+def exercise3():
+    pass
+#4. Read toothpaste sales data of each month and show it using a scatter plot
+
+def exercise4():
+    pass
+
+#5. Read sales data of bathing soap of all months and show it using a bar chart. Save this plot to your hard disk
+def exercise5():
+    pass
+#6. Read the total profit of each month and show it using the histogram to see most common profit ranges
+def exercise6():
+    pass
+#7. Read Bathing soap facewash of all months and display it using the Subplot
+def exercise7():
+    pass
