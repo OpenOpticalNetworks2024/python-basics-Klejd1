9647fa762fc589c5091032c7df2b74ea2396a307
Klejd1 <s335125@studenti.polito.it>
2024-10-19 17:42:45 +0200

    Set4

---
+++ Lab2/exercise_set5.py
@@ -0,0 +1,40 @@
+import json
+
+#1. Write a Python program to convert JSON data to Python objects.
+def exercise1():
+    states = json.loads(states)
+    print(states)
+#2. Write a Python program to convert Python objects (dictionary) to JSON data.
+def exercise2():
+    pass
+#3. Write a Python program to convert Python objects into JSON strings. Print all the values.
+
+def exercise3():
+    pass
+
+#4. Write a Python program to convert Python dictionary objects (sort by
+#key) to JSON data. Print the object members with indent level 4.
+def exercise4():
+    pass
+
+#5. Write a Python program to create a new JSON file from an existing JSON
+#file. Use the included json file ’states.json’ and create a new json file that
+#does not contain the ’area code’ field.
+def exercise5():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    print("EXERCISE SET 1")
+    print("EX1")
+    exercise1()
+    print("EX2")
+    exercise2()
+    print("EX3")
+    exercise3()
+    print("EX4")
+    exercise4()
+    print("EX5")
+    exercise5()
